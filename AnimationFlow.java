2e98b1d6c44bb4f8457b84d3c17840f27c173682
y-takaesu <y.taka.s0525@gmail.com>
2012-10-21 07:58:21 +0900

    同時実行に対応。インタフェースは要見直し

--- AnimationFlow.java
+++ AnimationFlow.java
@@ -1,6 +1,7 @@
 package hotpot.game.animation;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import android.view.View;
 import android.view.animation.Animation;
@@ -8,18 +9,41 @@ import android.view.animation.Animation.AnimationListener;
 
 public class AnimationFlow implements AnimationListener {
 
+	private ArrayList<AnimationList> animList2;
 	private ArrayList<AnimationItem> animList;
 	private int animationCount;
 	private CallbackListener callbackListener;
 
 	public AnimationFlow() {
-		animList = new ArrayList<AnimationItem>();
+		// animList = new ArrayList<AnimationItem>();
 		animationCount = 0;
+
+		animList2 = new ArrayList<AnimationFlow.AnimationList>();
+
 	}
 
 	public void add(View v, Animation anim) {
-		animList.add(new AnimationItem(v, anim));
+		// animList.add(new AnimationItem(v, anim));
+
+		AnimationList list = new AnimationList();
+		list.add(new AnimationItem(v, anim));
+		animList2.add(list);
 	}
+	
+	public void add(ArrayList<AnimationItem> animationItemArrayList){
+		AnimationList list = new AnimationList();
+		for(AnimationItem item : animationItemArrayList){
+			list.add(item);
+		}
+		animList2.add(list);
+	}
+	
+//	public void add(AnimationList animationList){
+//		animList2.add(animationList);
+//	}
+//	public AnimationList getAnimationList(){
+//		return animList2.get(0);
+//	}
 
 	public void setCallback(CallbackListener callbackListener) {
 		this.callbackListener = callbackListener;
@@ -34,15 +58,31 @@ public class AnimationFlow implements AnimationListener {
 	}
 
 	private void animationStart(int i) {
-		if (animList.size() <= i) {
+		// if (animList.size() <= i) {
+		if (animList2.size() <= i) {
 			animationEnd();
 			return;
 		}
-		AnimationItem item = animList.get(i);
+		// AnimationItem item = animList.get(i);
 
-		setListener(item.animation);
-
-		item.view.startAnimation(item.animation);
+		{
+			int longDurationItemId = 0;
+			for(int j=0; j < animList2.get(i).list.size(); j++){
+				AnimationItem item = animList2.get(i).list.get(j);
+				
+				if(animList2.get(i).list.get(longDurationItemId).animation.getDuration() < item.animation.getDuration()) {
+					longDurationItemId = j;
+				}
+			}
+			setListener(animList2.get(i).list.get(longDurationItemId).animation);
+			
+		}
+//			setListener(item.animation);
+//		item.view.startAnimation(item.animation);
+		for(int j=0; j < animList2.get(i).list.size(); j++){
+			AnimationItem item = animList2.get(i).list.get(j);
+			item.view.startAnimation(item.animation);
+		}
 	}
 
 	private void animationEnd() {
@@ -69,13 +109,25 @@ public class AnimationFlow implements AnimationListener {
 	public void onAnimationStart(Animation arg0) {
 	}
 
-	class AnimationItem {
+	public static class AnimationItem {
 		Animation animation;
 		View view;
 
 		public AnimationItem(View v, Animation anim) {
 			view = v;
 			animation = anim;
+		}
+	}
+
+	class AnimationList {
+		ArrayList<AnimationItem> list;
+
+		public AnimationList() {
+			list = new ArrayList<AnimationFlow.AnimationItem>();
+		}
+
+		public void add(AnimationItem item) {
+			list.add(item);
 		}
 	}
 
--- AnimationFlowActivity.java
+++ AnimationFlowActivity.java
@@ -1,6 +1,10 @@
 package hotpot.game.animation;
 
+import hotpot.game.animation.AnimationFlow.AnimationItem;
 import hotpot.game.animation.AnimationFlow.CallbackListener;
+
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -25,10 +29,19 @@ public class AnimationFlowActivity extends Activity {
         ScaleAnimation anim3 = new ScaleAnimation(1, 2, 1, 2);
         anim3.setDuration(5000);
         
+        
+        AnimationItem item = new AnimationItem(view1, anim1);
+        AnimationItem item2 = new AnimationItem(view2, anim2);
+        ArrayList<AnimationItem> animationItemArrayList = new ArrayList<AnimationFlow.AnimationItem>();
+        animationItemArrayList.add(item);
+        animationItemArrayList.add(item2);
+        
+        
         AnimationFlow animationFlow = new AnimationFlow();
         animationFlow.add(view1, anim1);
         animationFlow.add(view2, anim2);
         animationFlow.add(view1, anim3);
+        animationFlow.add(animationItemArrayList);
         animationFlow.setCallback(new CallbackListener() {
 			
 			@Override
